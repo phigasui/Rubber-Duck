524d2d93aae9450a588515b62b76303cbf16be59
phigasui <phigasui@gmail.com>
2018-09-08 03:09:08 +0900

    feature: show tweet

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
 import { h, app } from "hyperapp"
 
-const IMG_DIR = "images"
-const DUCK_IMG = "ishino_last.png"
+const PARAMS = {count: 1, screen_name: 'maina_5656'}
+const DEFAULT_IMAGE_SRC = "images/ishino_last.png"
 
 const IMG_WIDTH = 100
+const TWEET_INTERVAL = 1
 
 const INITIAL_MESSAGE = "おはようございます！"
-
-const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const MESSAGES = [
   "おつかれさまです！",
@@ -20,8 +19,7 @@ const state = {
     y: 0
   },
   message: INITIAL_MESSAGE,
-  isRecognition: false,
-  recognition: null
+  imageSrc: DEFAULT_IMAGE_SRC
 }
 
 const getRandomMessage = () => (
@@ -33,45 +31,11 @@ const actions = {
   setMessage: (message) => ({message: message}),
   start: () => (state, actions) => {
     console.log("start")
-    actions.recInit()
-    setInterval(
-      () => actions.setMessage(getRandomMessage()),
-      1000 * 60 * 60
-    )
-    setTimeout(actions.recStart, 3000)
+    actions.getTweet()
+    setInterval(actions.getTweet, 1000 * TWEET_INTERVAL)
   },
-  recInit: () => (state, actions) => {
-    let recognition = new SpeechRecognition()
-    recognition.lang = "ja-JP"
-    recognition.onend = () => {
-      actions.recStop()
-    }
-
-    recognition.onresult = (e) => {
-      console.log("kiteru")
-      if (e.results.length > 0) {
-        let value = e.results[0][0].transcript
-        actions.setMessage(value)
-      }
-    }
-
-    return {recognition: recognition}
-  },
-  recStart: () => (state, actions) => {
-    console.log("rec start")
-    state.recognition.start()
-
-    return {isRecognition: !state.isRecognition}
-  },
-  recStop: () => (state, actions) => {
-    console.log("rec stop")
-    let isRecognition = state.isRecognition
-    if (state.isRecognition) {
-      state.recognition.stop()
-      isRecognition = !isRecognition
-    }
-
-    return {isRecognition: isRecognition}
+  getTweet: () => (state, actions) => {
+    electronApp.getTimeline('user', PARAMS, actions.setMessage)
   }
 }
 
@@ -84,7 +48,7 @@ const Message = ({message}) => (
     width: 90,
     height: 90,
     background: "#70a6ff",
-    "border-radius": "50%",
+    "border-radius": "3%",
     color: "#EEE",
     "vertical-align": "middle",
     "text-align": "center",
@@ -108,28 +72,28 @@ const Message = ({message}) => (
 
 const Duck = () => (state, actions) => (
   <div
-  oncreate={actions.start}
-  style={{
-    position: "absolute",
-    right: state.position.x,
-    bottom: state.position.y,
-  }}
-  >
-  {() => {
-    if (state.message) return <Message message={state.message} />
-    return null
-  }}
-  <a href="javascript:void(0)">
-  <img
-  src={`${IMG_DIR}/${DUCK_IMG}`} width={IMG_WIDTH}
-  />
-  </a>
+    oncreate={actions.start}
+    style={{
+      position: "absolute",
+      right: state.position.x,
+      bottom: state.position.y
+    }}
+    >
+    {() => {
+      if (state.message) {
+        return <Message message={state.message} />
+      }
+      return null
+    }}
+    <a href="javascript:void(0)">
+      <img src={state.imageSrc} width={IMG_WIDTH} />
+    </a>
   </div>
 )
 
 const view = (state, actions) => (
   <main>
-  <Duck />
+    <Duck />
   </main>
 )
 
